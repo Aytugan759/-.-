84d980f514b4a8178073f4e420e288e71894a27d
Aytugan759 <aitugan.759@gmail.com>
2023-04-18 12:17:22 +0500

    #3 Добавил функцию для метода дихотомии

--- perebor.py
+++ perebor.py
@@ -19,4 +19,28 @@ print(j, f(x[j]), x[j])
 x_ = (x[j-1]+x[j+1])/2
 print('x* = ',x_)
 print('fact toch = ', abs(x_-3.1))
-print('teor toch =', (b-a)/(N+1))
+
+def dihot():
+    def f(x):
+        return 5 * x ** 2 - 5 + 31 * abs(x - 5)
+
+    a = -1
+    b = 6
+    N = 23
+    xa = a
+    xb = b
+    k = N // 2
+    d = (b - a) / (2 ** (k))
+
+    for i in range(k):
+        x1 = (a + xb) / 2 - d
+        x2 = (xa + b) / 2 + d
+
+        if f(x1) <= f(x2):
+            xb = x2
+        else:
+            xa = x1
+    x_ = (xa + xb) / 2
+    l = (b - a) / (2 ** (k)) + d * (2 ** k - 1) / (2 ** (k - 1))
+    print('x*=', x_, 'дельта = ', d, ' теоретическая погрешность = ', l / 2, " фактическая погрешность = ",
+          abs(x_ - 3.1))
